ec3af988f5778e25a997952cdbdeab0953a0b47e
Erko <matteo.ercolani@gmail.com>
2016-11-19 21:07:52 +0100

    map random results

--- app/components/SearchBar/index.js
+++ app/components/SearchBar/index.js
@@ -10,21 +10,30 @@ import {searchBusinessExpertise, loadBusinessExpertise} from 'containers/SearchP
 
 class SearchBar extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
-  handleChange = (value) => {
 
-    value = 5;
+  state = {
+    expertiseId: 1
+  };
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  handleChange = (value) => {
+    this.setState({expertiseId: value});
+
+    value = this.getRandomInt(1, 15);
 
     let action = searchBusinessExpertise(value);
     this.props.dispatch(action);
     action = loadBusinessExpertise();
     this.props.dispatch(action);
-
   };
-
 
   render() {
     return (
-      <Autocomplete label={'Search what you want'} onChange={this.handleChange}/>
+      <Autocomplete label={'Search what you want'} onChange={this.handleChange} value={this.state.expertiseId}/>
     );
   }
 }
